1dda35f39b6f1786816c6b32654c495541497625
Whishes <nicholas.cruickshank25@gmail.com>
2021-08-13 15:36:38 +1000

    Update LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
-import { useDispatch } from "react-redux"
-import loginService from "../services/login"
-import blogService from "../services/blogs"
-import { successMessage, errorMessage } from "../reducers/notificationReducer"
-import { logUser } from "../reducers/userReducer"
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import loginService from "../services/login";
+import blogService from "../services/blogs";
+import { successMessage, errorMessage } from "../reducers/notificationReducer";
+import { logUser } from "../reducers/userReducer";
 import {
   TextField,
   Button,
@@ -11,7 +11,7 @@ import {
   Container,
   makeStyles,
   CssBaseline,
-} from "@material-ui/core"
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -27,32 +27,32 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}))
+}));
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const dispatch = useDispatch()
-  const classes = useStyles()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
       const user = await loginService.login({
         username,
         password,
-      })
+      });
 
-      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user))
-      blogService.setToken(user.token)
-      dispatch(logUser(user))
-      setUsername("")
-      setPassword("")
-      dispatch(successMessage(`${user.name} logged in successfully`))
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      dispatch(logUser(user));
+      setUsername("");
+      setPassword("");
+      dispatch(successMessage(`${user.name} logged in successfully`));
     } catch (exception) {
-      dispatch(errorMessage(exception.response.data.error))
+      dispatch(errorMessage(exception.response.data.error));
     }
-  }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -60,6 +60,9 @@ const LoginForm = () => {
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
           Log in
+        </Typography>
+        <Typography component="h2" variant="h5">
+          For now - use Test for user & pass
         </Typography>
         <form className={classes.form} onSubmit={handleLogin}>
           <TextField
@@ -98,7 +101,7 @@ const LoginForm = () => {
         </form>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
