fc44e5ba4b623f7f8a71bad23d0e0dc839a7f283
Whishes <nicholas.cruickshank25@gmail.com>
2021-08-13 15:24:44 +1000

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    "jest/globals": true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
